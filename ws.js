1c14a975ea29707abaf18ac3ad52398087dbcd18
Jeff Davies <hipparchus2000@gmail.com>
2017-08-21 01:15:06 +0100

    intervalfunc

--- ws.js
+++ ws.js
@@ -8,28 +8,29 @@ var clients = [];
 var ws=require("nodejs-websocket");
 
 ws.createServer(function (websocket) {
-	setInterval(intervalFunc, 5000);
-websocket.addListener("connect", 
-	function (resource) { 
-		sys.debug("connect: " + resource);
-		clients.push(websocket);
-		// setTimeout(websocket.end, 10 * 1000); 
-	}).addListener("data", function (data) { 
-		sys.debug(data);
-		
-		for(var i = 0; i < clients.length; i++) {
-			clients[i].write("Hello");
-		}
-	}).addListener("close", function () { 
-		sys.debug("close");
-		for(var i = 0; i < clients.length; i++) {
-			if(clients[i] == websocket) {
-				clients.splice(i);
-				break;
+	websocket.addListener("connect", 
+		function (resource) { 
+			sys.debug("connect: " + resource);
+			clients.push(websocket);
+			// setTimeout(websocket.end, 10 * 1000); 
+		}).addListener("data", function (data) { 
+			sys.debug(data);
+			
+			for(var i = 0; i < clients.length; i++) {
+				clients[i].write("Hello");
 			}
-		}
+		}).addListener("close", function () { 
+			sys.debug("close");
+			for(var i = 0; i < clients.length; i++) {
+				if(clients[i] == websocket) {
+					clients.splice(i);
+					break;
+				}
+			}
 	});
 }).listen(13003); 
+
+setInterval(intervalFunc, 5000);
 
 function intervalFunc() {
 	
